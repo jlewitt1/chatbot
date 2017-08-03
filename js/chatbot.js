f88d1aa81f812519fbc9aea5299523c185ff411b
Josh <josh.lewittes@gmail.com>
2017-08-03 14:40:47 +0300

    fix server path

--- js/chatbot.js
+++ js/chatbot.js
@@ -2,7 +2,7 @@ var ChatBot = {};
 
 //The server path will be used when sending the chat message to the server.
 //todo replace with your server path if needed
-ChatBot.SERVER_PATH = "https://radiant-hollows-65067.herokuapp.com/";
+ChatBot.SERVER_PATH = "https://radiant-hollows-65067.herokuapp.com";
 ChatBot.DEFAULT_ANIMATION = "waiting";
 //The animation timeout is used to cut the current running animations when a new animations starts
 ChatBot.animationTimeout;
